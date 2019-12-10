50f2542ad690bd306f97817b4a6686e34409f66a
TomoyoUchima <tomoyo.uchima@cydas.com>
2019-12-10 16:27:37 +0900

    クリックイベント(v-on),preventDefault

--- main.js
+++ main.js
@@ -9,11 +9,22 @@
 var vm = new Vue({
   el: "#app",
   data: {
+    newItem: '',
     todos: [
       'task1',
       'task2',
       'task3',
     ]
+  },
+  methods: {
+    // addItem: function(e){
+    //   e.preventDefault(); //規定のページが遷移することを無効化する
+    //   this.todos.push(this.newItem);
+    addItem: function(){
+      this.todos.push(this.newItem);
+      this.newItem = '';
+    
+    }
   }
 })
 
